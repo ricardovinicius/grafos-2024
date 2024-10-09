f3cffe7b1f2fe573790f149f4432271e084d7591
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2024-10-09 17:13:48 -0300

    feat: add kosaraju

--- kosaraju/kosaraju.cpp
+++ kosaraju/kosaraju.cpp
@@ -1,107 +1,121 @@
 #include <iostream>
 #include <list>
+#include <map>
 #include <stack>
-#include <vector>
-
 using namespace std;
 
 class Graph {
-    int V;      
-    vector<int> *adj;
-    void DFSUtil(int v, bool visited[], bool output, stack<int> *Stack = nullptr);
+    int V;
+
+    map<int, list<int> >
+        adjList; // Adjacency list to store the graph
 
 public:
-    Graph(int V);
-    void addEdge(int v, int w);
-    Graph getTranspose();
-    void findSCCs();
-};
-
-Graph::Graph(int V) {
-    this->V = V;
-    adj = new vector<int>[V];
-}
-
-void Graph::addEdge(int v, int w) {
-    adj[v].push_back(w);
-}
-
-void Graph::DFSUtil(int v, bool visited[], bool output, stack<int> *Stack) {
-    visited[v] = true;
-    
-    if (output) 
-        cout << v << " ";
-    
-    cout << v;
-
-    for (int i : adj[v]) {
-        if (!visited[i])
-            DFSUtil(i, visited, output, Stack);
+    Graph(int V) {
+        this->V = V;
     }
 
-    if (Stack != nullptr) {
-        Stack->push(v);
-    }
-}
-
-Graph Graph::getTranspose() {
-    Graph g_t(V);
-
-    for (int v = 0; v < V; v++) {
-        for (int i : adj[v]) {
-            g_t.adj[i].push_back(v);
-        }
+    // Function to add an edge between vertices u and v of
+    // the graph
+    void add_edge(int u, int v)
+    {
+        adjList[u].push_back(v);
     }
 
-    return g_t;
-}
-
-void Graph::findSCCs() {
-    stack<int> Stack;
-
-    bool *visited = new bool[V];
-    for (int i = 0; i < V; i++) {
-        visited[i] = false;
-    }
-
-    for (int i = 0; i < V; i++) {
-        if (visited[i] == false) {
-            DFSUtil(i, visited, false, &Stack);
-        }
-    }
-
-    Graph g_t = getTranspose();
-
-    for (int i = 0; i < V; i++) {
-        visited[i] = false;
-    }
-
-    while (!Stack.empty()) {
-        int v = Stack.top();
-        Stack.pop();
-
-        if (!visited[v]) {
-            g_t.DFSUtil(v, visited, true);
+    // Function to print the adjacency list representation
+    // of the graph
+    void print()
+    {
+        cout << "Adjacency list for the Graph: " << endl;
+        // Iterate over each vertex
+        for (auto i : adjList) {
+            // Print the vertex
+            cout << i.first << " -> ";
+            // Iterate over the connected vertices
+            for (auto j : i.second) {
+                // Print the connected vertex
+                cout << j << " ";
+            }
             cout << endl;
         }
     }
 
-    delete[] visited;
-}
+    void DFSUtil(map<int, list<int>>adjList, int v, bool visited[], stack<int> *Stack)
+    {
+        visited[v] = true;
+        
+        if (Stack == nullptr) {
+            cout << v << " ";
+        }
+            
+        for (int i : adjList[v])
+            if (visited[i] == false)
+                DFSUtil(adjList, i, visited, Stack);
 
-int main() {
-    int v_size, e_size;
-
-    cin >> v_size >> e_size;
-
-    Graph g(v_size);
-
-    for (int i = 0; i < e_size; i++) {
-        int v_1, v_2;
-        cin >> v_1 >> v_2;
-        g.addEdge(v_1, v_2);
+        if (Stack != nullptr) {
+            Stack->push(v);
+        }
     }
 
-    cout << "Componentes fortemente conectados no grafo:\n";
+    Graph getTranspose() {
+        Graph g(V);
+
+        for (int v = 1; v < V + 1; v++) {
+            for (int i : adjList[v]) {
+                g.add_edge(i, v);
+            }
+        }
+
+        return g;
+    }
+
+    void findSCCs() {
+        stack<int> Stack;
+
+        bool visited[V + 1];
+        for (int i = 1; i < V + 1; i++)
+            visited[i] = false;
+
+        for (int i = 1; i < V + 1; i++) {
+            if (!visited[i]) {
+                DFSUtil(adjList, i, visited, &Stack);
+                cout << endl;
+            }
+        }    
+
+        Graph g_t = getTranspose();
+
+        for (int i = 1; i < V + 1; i++)
+            visited[i] = false;
+
+        while (!Stack.empty()) {
+            int v = Stack.top();
+            Stack.pop();
+
+            if (!visited[v]) {
+                g_t.DFSUtil(g_t.adjList, v, visited, nullptr);
+                cout << endl;
+            }
+        }
+    }
+};
+
+int main()
+{
+    int vertices, edges;
+    cin >> vertices >> edges;
+
+    Graph g(vertices);
+
+    cout << vertices << " " << edges << endl;
+
+    for (int i = 0; i < edges; i++) {
+        int vertice_1, vertice_2;
+        cin >> vertice_1 >> vertice_2;
+
+        g.add_edge(vertice_1, vertice_2);
+    }
+
     g.findSCCs();
+    return 0;
 }
