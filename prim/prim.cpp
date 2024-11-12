c3152f9f92561520d7e0d9be4846e2c18cbc8045
Ricardo Vinicius <108153768+ricardovinicius@users.noreply.github.com>
2024-11-12 17:57:50 -0300

    working on prim 2

--- prim/prim.cpp
+++ prim/prim.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#define INF 2147483647
 using namespace std;
 
 class UWGraph // undirected weight graph
@@ -7,82 +8,119 @@ class UWGraph // undirected weight graph
     vector<vector<pair<int, int>>> adj; // lista de adjacencia com par (vertice, custo)
 
 public:
-
-    UWGraph(int V) {
+    UWGraph(int V)
+    {
         this->V = V;
         adj.resize(V);
     }
 
-    void addEdge(int u, int v, int wt) {
+    void addEdge(int u, int v, int wt)
+    {
         adj[u].push_back(make_pair(v, wt));
         adj[v].push_back(make_pair(u, wt));
     }
 
-    void printGraph() {
-        for (int u = 0; u < V; u++) {
+    void printGraph()
+    {
+        for (int u = 0; u < V; u++)
+        {
             cout << "Edges of " << u << ":" << endl;
 
-            for (auto v : adj[u]) {
+            for (auto v : adj[u])
+            {
                 cout << "\tNode " << v.first << " with weight " << v.second << endl;
             }
         }
     }
 
-    vector<int> mst() {
-        vector<int> pa(this->V);
-        for (int i = 0; i < V; i++) {
-            pa[i] = -1;
+    pair<vector<int>, int> prim(int v_0)
+    {
+        vector<int> custo(this->V);
+        vector<int> prev(this->V);
+
+        for (int i = 0; i < V; i++)
+        {
+            custo[i] = INF;
+            prev[i] = -1;
         }
 
-        pa[0] = 0;
-        while (true) {
-            int min = 2147483647;
+        custo[v_0] = 0;
 
-            int x, y;
-            for (int i = 0; i < this->V; i++) {
-                if (pa[i] == -1) continue;
-                for (auto v : adj[i]) {
-                    if (pa[v.first] == -1 && v.second < min) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        vector<bool> visited(this->V, false);
 
-                        min = v.second;
-                        x = i, y = v.first;
-                    }
-                }
+        for (int i = 0; i < custo.size(); i++)
+        {
+            pq.push(make_pair(i, custo[i]));
+        }
+
+        int wt_sum = 0;
+
+        while (!pq.empty())
+        {
+            pair<int, int> v = pq.top();
+            pq.pop();
+
+            int u = v.first;
+            int wt = v.second;
+
+            if (visited[u] == true)
+            {
+                continue;
             }
 
-            if (min == 2147483647) break;
-            pa[y] = x;
-        } 
+            wt_sum += wt;
+            cout << wt << endl;
+            visited[u] = true;
 
-        return pa;
+            for (auto w : adj[u])
+            {
+                if (visited[w.first] == false && custo[w.first] > w.second)
+                {
+                    custo[w.first] = w.second;
+                    pq.push(make_pair(w.first, w.second));
+                    prev[w.first] = v.first;
+                }
+            }
+        }
+
+        return make_pair(prev, wt_sum);
     }
 };
 
-int main() {
-    int V = 8;
-    UWGraph graph(V);
+int main()
+{
+    int V = 8; // Temos 8 vértices (0 a 7)
 
-    graph.addEdge(5, 4, 35);
-    graph.addEdge(7, 4, 37);
-    graph.addEdge(7, 5, 28);
-    graph.addEdge(0, 7, 16);
-    graph.addEdge(1, 5, 32);
-    graph.addEdge(0, 4, 38);
-    graph.addEdge(2, 3, 17);
-    graph.addEdge(7, 1, 19);
-    graph.addEdge(0, 2, 26);
-    graph.addEdge(1, 2, 36);
-    graph.addEdge(1, 3, 29);
-    graph.addEdge(7, 2, 34);
-    graph.addEdge(2, 6, 40);
-    graph.addEdge(3, 6, 52);
-    graph.addEdge(0, 6, 58);
-    graph.addEdge(6, 4, 93);
-    graph.printGraph();
+    UWGraph g(V);
 
-    auto res = graph.mst();
-    
-    for (int i = 0; i < res.size(); i++) {
-        cout << i << "->" << res[i] << " ";
+    // Adicionando as arestas e seus pesos
+    g.addEdge(5, 4, 35); // Aresta 5-4 com peso 35
+    g.addEdge(7, 4, 37); // Aresta 7-4 com peso 37
+    g.addEdge(7, 5, 28); // Aresta 7-5 com peso 28
+    g.addEdge(0, 7, 16); // Aresta 0-7 com peso 16
+    g.addEdge(1, 5, 32); // Aresta 1-5 com peso 32
+    g.addEdge(0, 4, 38); // Aresta 0-4 com peso 38
+    g.addEdge(2, 3, 17); // Aresta 2-3 com peso 17
+    g.addEdge(7, 1, 19); // Aresta 7-1 com peso 19
+    g.addEdge(0, 2, 26); // Aresta 0-2 com peso 26
+    g.addEdge(1, 2, 36); // Aresta 1-2 com peso 36
+    g.addEdge(1, 3, 29); // Aresta 1-3 com peso 29
+    g.addEdge(7, 2, 34); // Aresta 7-2 com peso 34
+    g.addEdge(2, 6, 40); // Aresta 2-6 com peso 40
+    g.addEdge(3, 6, 52); // Aresta 3-6 com peso 52
+    g.addEdge(0, 6, 58); // Aresta 0-6 com peso 58
+    g.addEdge(6, 4, 93); // Aresta 6-4 com peso 93
+
+    auto res = g.prim(0);
+    auto tree = res.first;
+    int wt_sum = res.second;
+
+    for (int i = 1; i < tree.size(); i++)
+    {
+        cout << i << "->" << tree[i] << " ";
     }
+
+    cout << endl
+         << "soma dos pesos: " << wt_sum << endl;
 }
